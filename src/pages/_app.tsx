8b9bffec45a857a7b20c6531a05b12ec0304f593
paulo-fs <jr.paulo87@gmail.com>
2022-09-14 11:16:43 -0300

    structure: configuring the base pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Layout } from 'src/components';
+import { ContextProvider } from 'src/shared/context';
+import { defaultTheme } from 'src/shared/styles/theme/defaultTheme';
+import { ThemeProvider } from 'styled-components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<ContextProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ContextProvider>
+		</ThemeProvider>
+	);
 }
 
 export default MyApp;
